9b342761f865c7a47c68f5480099f6da7f995f54
olliklee <okleemann@gmail.com>
2024-08-31 14:10:55 +0200

    WiP

--- lib/tools/uncategorized/text_magic/logic/text_magic.dart
+++ lib/tools/uncategorized/text_magic/logic/text_magic.dart
@@ -3,10 +3,19 @@ enum TEXTMAGIC_OPTION {
   LINESTART, WORDSTART, SENTENCESTART, PUNCTUATIONMARK,
   CHARACTER, UPPER_CHAR, DIGIT, NUMBER, TAG }
 
-RegExp _createRegEx(TEXTMAGIC_OPTION option, {int charCount = 1, String htmlTag = 'em', String specialChar = 'a'}) {
+class RegExpGroup {
+  final RegExp regExp;
+  final int group;
+
+  RegExpGroup(this.regExp, this.group);
+}
+
+RegExpGroup _createRegExpGroup(TEXTMAGIC_OPTION option, {int charCount = 1, String? htmlTag = 'em', String? specialChar = 'a'}) {
   late String pattern;
+  int group = 0;
+
   switch (option) {
-    case TEXTMAGIC_OPTION.LINESTART:
+    case TEXTMAGIC_OPTION.LINESTART: // check
       pattern = '^.{$charCount}';
       break;
     case TEXTMAGIC_OPTION.WORDSTART:
@@ -19,7 +28,8 @@ RegExp _createRegEx(TEXTMAGIC_OPTION option, {int charCount = 1, String htmlTag 
       pattern = '(^|(?<=[.!?,;-_"\'/()]\\s))\\w{$charCount}';
       break;
     case TEXTMAGIC_OPTION.CHARACTER:
-      pattern = '$specialChar{$charCount}';
+      pattern = '$specialChar{$charCount}(?=(.{$charCount}))';
+      group = 1;
       break;
     case TEXTMAGIC_OPTION.UPPER_CHAR:
       pattern = '[A-Z]';
@@ -32,23 +42,43 @@ RegExp _createRegEx(TEXTMAGIC_OPTION option, {int charCount = 1, String htmlTag 
       break;
     case TEXTMAGIC_OPTION.TAG:
       pattern = '<$htmlTag>([^<]+)</$htmlTag>';
+      group = 1;
       break;
   }
 
-  return RegExp(pattern, multiLine: (option == TEXTMAGIC_OPTION.LINESTART));
+  return RegExpGroup(RegExp(pattern, multiLine: (option == TEXTMAGIC_OPTION.LINESTART)), group);
 }
 
 void main() {
-  int anzZeichen = 1;
-  String text = 'Dies ist ein BeispielText <em>MIT</em> Großbuchstaben. Und <em> tags</em>';
-  TEXTMAGIC_OPTION option = TEXTMAGIC_OPTION.CHARACTER;
+  String text = 'Dies ist ein BeispielText <em>MIT</em> Großbuchstaben. Und 2 <em> tags</em>. Insgesamt 231!';
+  print(text);
+  Map<TEXTMAGIC_OPTION, List<dynamic>> parameters = {
+    TEXTMAGIC_OPTION.LINESTART: [2, '', ''],
+    TEXTMAGIC_OPTION.WORDSTART: [2, '', ''],
+    TEXTMAGIC_OPTION.SENTENCESTART: [3, '', ''],
+    TEXTMAGIC_OPTION.PUNCTUATIONMARK: [1, '', ''],
+    TEXTMAGIC_OPTION.CHARACTER: [1, '', 'e'],
+    TEXTMAGIC_OPTION.UPPER_CHAR: [1, '', ''],
+    TEXTMAGIC_OPTION.DIGIT: [1, '', ''],
+    TEXTMAGIC_OPTION.NUMBER: [1, '', ''],
+    TEXTMAGIC_OPTION.TAG: [1 , 'strong', ''],
+  };
 
-  var regExp = _createRegEx(option, htmlTag: 'em');
-  Iterable<RegExpMatch> matches = regExp.allMatches(text);
+  for (var mode in parameters.keys) {
+    var regExpGroup = _createRegExpGroup(
+        mode,
+        charCount: parameters[mode]?[0] as int,
+        htmlTag: parameters[mode]?[1] as String?,
+        specialChar: parameters[mode]?[2] as String?
+    );
+    Iterable<RegExpMatch> matches = regExpGroup.regExp.allMatches(text);
 
-  StringBuffer founds = StringBuffer();
-  for (final match in matches) {
-    founds.write(match.group(0));
+    StringBuffer founds = StringBuffer();
+    for (final match in matches) {
+      founds.write(match.group(regExpGroup.group));
+    }
+
+    print('Mode: $mode + Prams: ${parameters[mode].toString()}' );
+    print('Found: ${founds.toString()}\n');
   }
-  print(founds.toString());
 }
